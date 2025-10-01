d591bffafe9c9eebd31960cfbf9a5dcd2954e22d
ali-sayed700 <aly.sayed9595@gmail.com>
2025-10-01 12:33:49 +0300

    csrd

--- apps/back/src/app.module.ts
+++ apps/back/src/app.module.ts
@@ -18,6 +18,7 @@ import { ConfigModule } from '@nestjs/config';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       path: '/graphql',
       driver: ApolloDriver,
+      csrfPrevention: false,
       autoSchemaFile: join(process.cwd(), 'src/graphql/schema.gql'),
     }),
     PrismaModule,
